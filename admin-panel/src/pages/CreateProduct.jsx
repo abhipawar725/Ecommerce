5723bebf42fcfa73bb06d6e9983b845c4bcccfcd
AbhishekPanwar <abhishekpawar725@gmail.com>
2025-05-28 07:10:51 +0530

    signup function created

--- admin-panel/src/pages/CreateProduct.jsx
+++ admin-panel/src/pages/CreateProduct.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import React from 'react'
-
+import { useState } from 'react'
 
 const CreateProduct = () => {
+  const [title, setTitle] = useState("")
+  const [price, setPrice] = useState("") 
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -14,6 +16,8 @@ const CreateProduct = () => {
     axios.post("http://localhost:2804/api/products", data)
       .then((res) => {
         alert("Product added successfully!");
+        setTitle("")
+        setPrice("")
       })
       .catch((err) => {
         alert("Error: " + (err?.response?.data?.message || "Something went wrong"));
@@ -28,11 +32,11 @@ const CreateProduct = () => {
           <input type="hidden" name='hidden_input' />
           <div className='flex flex-col gap-2'>
             <label htmlFor="product_name">Product Name</label>
-            <input type="text" name='product_name' className='h-12 w-full rounded border border-slate-300 px-4 outline-0' />
+            <input type="text" name='product_name' className='h-12 w-full rounded border border-slate-300 px-4 outline-0' value={title} onChange={(e) => setTitle(e.target.value)} />
           </div>
           <div className='flex flex-col gap-2'>
             <label htmlFor="product_price">Product Price</label>
-            <input type="number" name='product_price' className='h-12 w-full rounded border border-slate-300 px-4 outline-0' />
+            <input type="number" name='product_price' className='h-12 w-full rounded border border-slate-300 px-4 outline-0' value={price} onChange={(e) => setPrice(e.target.price)} />
           </div>
           <div className='flex flex-col gap-2'>
             <button type='submit' className='h-12 w-full rounded bg-slate-900 text-white px-4 outline-0'>Submit</button>
